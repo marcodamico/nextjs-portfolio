b937d5830a79b2f311845f3c77a6d9dcc2d563f6
Marco D'Amico <marcodamico@protonmail.com>
2024-08-17 20:26:35 +0200

    Added images, updated links

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -23,8 +23,8 @@ export async function generateMetadata({
     image,
   } = post.metadata;
   let ogImage = image
-    ? `https://leerob.io${image}`
-    : `https://leerob.io/og?title=${title}`;
+    ? `https://marcodamico.github.io${image}`
+    : `https://marcodamico.github.io/og?title=${title}`;
 
   return {
     title,
@@ -34,7 +34,7 @@ export async function generateMetadata({
       description,
       type: 'article',
       publishedTime,
-      url: `https://leerob.io/blog/${post.slug}`,
+      url: `https://marcodamico.github.io/blog/${post.slug}`,
       images: [
         {
           url: ogImage,
@@ -103,12 +103,12 @@ export default function Blog({ params }) {
             dateModified: post.metadata.publishedAt,
             description: post.metadata.summary,
             image: post.metadata.image
-              ? `https://leerob.io${post.metadata.image}`
-              : `https://leerob.io/og?title=${post.metadata.title}`,
-            url: `https://leerob.io/blog/${post.slug}`,
+              ? `https://marcodamico.github.io${post.metadata.image}`
+              : `https://marcodamico.github.io/og?title=${post.metadata.title}`,
+            url: `https://marcodamico.github.io/blog/${post.slug}`,
             author: {
               '@type': 'Person',
-              name: 'Lee Robinson',
+              name: 'Marco D\'Amico',
             },
           }),
         }}
