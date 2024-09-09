b766f9881a6fc7759b4af2bccab37d74da205f8c
marcodamico <marcodamico@protonmail.com>
2024-09-09 17:11:32 +0200

    Avoid using next/server in the cron API

--- api/cron.ts
+++ api/cron.ts
@@ -1,15 +1,17 @@
-import { NextResponse } from 'next/server';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
 import { getViewsCount } from 'app/db/queries';
 
-export default async function handler() {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
         console.log("Cron job started"); // Log start
         let views = await getViewsCount();
-    
+        console.log("Cron job succeded");
         // Return the fetched data
-        return NextResponse.json({ message: 'Data fetched successfully!', views });
+        res.status(200).json({ message: 'Data fetched successfully!', views });
       } catch (error) {
         // Handle any errors
-        return NextResponse.json({ message: 'Error fetching data', error: error.message });
+        console.log("Cron job failed");
+        res.status(200).json({ message: 'Error fetching data', error: error.message });
       }
 }
