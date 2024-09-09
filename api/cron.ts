61e0c19fc57e28109d010293a9c44400fbceaf37
marcodamico <marcodamico@protonmail.com>
2024-09-09 18:35:37 +0200

    Fixed path to db queries in cron.ts

--- api/cron.ts
+++ api/cron.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-import { getViewsCount } from 'app/db/queries';
+import { getViewsCount } from '../app/db/queries';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
