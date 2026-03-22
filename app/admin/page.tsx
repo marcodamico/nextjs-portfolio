1c552f901206dae177b149cac725aba7b401099a
marcodamico <marcodamico@protonmail.com>
2026-03-22 16:27:40 +0100

    More fixes on fonts

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,5 @@
 import { auth } from 'app/auth';
-import { getGuestbookEntries } from 'app/db/queries';
+import { getGuestbookEntries } from '../db/queries';
 import { redirect } from 'next/navigation';
 import Form from './form';
 
